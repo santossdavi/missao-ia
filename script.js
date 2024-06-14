8fc83176801aec398a7822448220b9d6b43b1d34
santossdavi <108529853+santossdavi@users.noreply.github.com>
2024-06-14 11:20:43 -0300

    yhh

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal =document.querySelector(".caixa-peincipal");
+const caixaPerguntas =document.querySelector(".caixa-perguntas");
+const caixaAlternativas =document.querySelector(".caixa-alternativas");
+const caixaResultado =document.querySelector(".caixa-resultado");
+const caixaResultado =document.querySelector(".caixa-resultado");
