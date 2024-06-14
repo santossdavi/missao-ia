12d7dfc91d323672fad9a0c9b9e88d0172d40050
santossdavi <108529853+santossdavi@users.noreply.github.com>
2024-06-14 11:36:42 -0300

    k

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const caixaPerguntas =document.querySelector(".caixa-perguntas");
 const caixaAlternativas =document.querySelector(".caixa-alternativas");
 const caixaResultado =document.querySelector(".caixa-resultado");
 const caixaResultado =document.querySelector(".caixa-resultado");
+
+const = perguntas [
+{
+        
+    enunciado: "Pergunta 1",
+    alternativas: [
+             "Alternativa 1",
+       "Alternativa 2"
+    ]
+}
+  
+{
+    enunciado: "Pergunta 2",
+    alternativas: [
+                 "Alternativa 1",
+       "Alternativa 2"
+    ]
+}
+    
+        
+ 
